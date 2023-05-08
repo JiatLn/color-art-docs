4b053b5b473aab50d08578a7daae97465e282488
林佳涛 <lne128gene980@163.com>
2023-05-08 11:02:54 +0800

    chore: app config update

--- app.config.ts
+++ app.config.ts
@@ -2,13 +2,24 @@ export default defineAppConfig({
   docus: {
     title: 'Color Art',
     description: 'Color Art documentation.',
+    url: 'https://color-art.netlify.app',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
       github: 'JiatLn/color-art'
     },
     aside: {
       level: 0,
+      collapsed: false,
       exclude: []
+    },
+    layout: "default",
+    github: {
+      root: 'content',
+      edit: true,
+      contributors: true,
+      repo: "color-art-docs",
+      branch: "main",
+      owner: "JiatLn",
     },
     header: {
       logo: true
