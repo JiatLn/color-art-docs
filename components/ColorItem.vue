1d36b96ce6e7f84ebcf9be02f89014cb94121c37
林佳涛 <lne128gene980@163.com>
2023-05-18 15:33:32 +0800

    refactor: ColorTrans compnent

---
+++ components/ColorItem.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="color-item " :title="colorStr">
+    <div class="content" :style="{ backgroundColor: colorStr }">
+      {{ colorStr }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { toRefs } from 'vue';
+
+const props = defineProps<{
+  colorStr: string,
+}>()
+const { colorStr } = toRefs(props)
+</script>
+
+<style scoped>
+.color-item {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  background: #fff;
+  border-radius: 4px;
+}
+
+.color-item .content {
+  z-index: 2;
+  min-width: 120px;
+  padding: 10px 20px;
+  color: #000;
+  text-align: center;
+}
+
+.color-item::after {
+  content: "";
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(135deg, #d4d4d4 25%, transparent 25%, transparent 75%, #d4d4d4 75%,
+      #d4d4d4 100%), linear-gradient(135deg, #d4d4d4 25%, transparent 25%, transparent 75%, #d4d4d4 75%, #d4d4d4 100%);
+  background-position: 0 0, 10px 10px;
+  background-size: 20px 20px;
+}
+</style>
--- components/ColorTrans.vue
+++ components/ColorTrans.vue
@@ -1,17 +1,18 @@
 <template>
-  <div class="line">
+  <div class="warpper">
     <div v-if="Array.isArray(input)" class="color-inputs">
-      <div class="input" :title="input[0]" :style="{ backgroundColor: input[0] }">{{ input[0] }}</div>
-      <span class="plus">+</span>
-      <div class="input" :title="input[1]" :style="{ backgroundColor: input[1] }">{{ input[1] }}</div>
+      <ColorItem :colorStr="input[0]" />
+      <span class="plus"> + </span>
+      <ColorItem :colorStr="input[1]" />
     </div>
-    <div v-else class="input" :title="input" :style="{ backgroundColor: input }">{{ input }}</div>
-    <span class="arrow">-></span>
-    <div class="output" :title="output" :style="{ backgroundColor: output }">{{ output }}</div>
+    <ColorItem v-else :colorStr="input" />
+    <span class="arrow"> -> </span>
+    <ColorItem :colorStr="output" />
   </div>
 </template>
 
 <script setup lang="ts">
+import ColorItem from './ColorItem.vue';
 import { toRefs } from 'vue';
 
 const props = defineProps<{
@@ -23,7 +24,7 @@ const { input, output } = toRefs(props)
 </script>
 
 <style scoped>
-.line {
+.warpper {
   display: flex;
   justify-content: center;
   align-items: center;
@@ -33,17 +34,6 @@ const { input, output } = toRefs(props)
 .arrow,
 .plus {
   padding: 8px 20px;
-}
-
-.input,
-.output {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  min-width: 120px;
-  padding: 10px 20px;
-  color: #000;
-  border-radius: 4px;
 }
 
 .color-inputs {
